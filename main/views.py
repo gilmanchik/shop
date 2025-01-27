04e9e5aa3b5da3aca770b6c0ca49c083a6c87b14
django-market <django@mail.com>
2025-01-27 14:05:20 +0500

    first commit

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse('Home page')
+
+
+def about(request):
+    print(request)
+    return HttpResponse('About page')
