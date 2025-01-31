9bb64a63d95934c2cf0e38c6f69f2db2db7d4463
django-market <django@mail.com>
2025-01-31 11:10:31 +0500

    add fixtures to goods

---
+++ goods/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import *
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import *
+from .views import *
 
 app_name = 'main'
 
