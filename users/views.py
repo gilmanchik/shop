bee03471f4a8f885c88c368215b362ee0e786833
django-market <django@mail.com>
2025-02-08 18:27:38 +0500

    end first part

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.contrib import auth
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
@@ -15,6 +16,9 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f'Добрый день {username}')
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
                 return HttpResponseRedirect(reverse('main:home'))
     else:
         form = UserLoginForm()
@@ -31,6 +35,7 @@ def registration(request):
             form.save()
             user = form.instance
             auth.login(request, user)
+            messages.success(request, f'Поздравляем с регистрацией {user.username}')
             return HttpResponseRedirect(reverse('main:home'))
     else:
         form = UserRegistrationForm()
@@ -40,11 +45,13 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
         if form.is_valid():
             form.save()
+            messages.success(request, f'Профиль успешно обновлен')
             return HttpResponseRedirect(reverse('users:profile'))
     else:
         form = ProfileForm(instance=request.user)
@@ -54,6 +61,8 @@ def profile(request):
     return render(request, 'users/profile.html', context)
 
 
+@login_required
 def logout(request):
+    messages.success(request, f'Досвидания {request.user.username}')
     auth.logout(request)
     return redirect(reverse('main:home'))
