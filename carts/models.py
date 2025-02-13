f12cf035dc020e0c036cca9beefc30376dedf50a
django-market <django@mail.com>
2025-02-13 07:45:15 +0500

    начал делать модель корзины

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from goods.models import Products
+from users.models import User
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    products = models.ForeignKey(Products, on_delete=models.CASCADE, verbose_name='Товар')
+    quantity = models.PositiveSmallIntegerField(default=0, verbose_name='Количество')
+    created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
+
+    class Meta:
+        db_table = 'cart'
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзина'
+
+    def product_price(self):
+        return round(self.products.sell_price() * self.quantity, 2)
+
+    def __str__(self):
+        return f'Корзина {self.user} | Товар {self.products} | Количество {self.quantity}'
