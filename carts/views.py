b4c49db38ada6d4809b759cc5e5075d7cb832d89
django-market <django@mail.com>
2025-03-02 21:08:58 +0500

    add ajax for add product in cart without update page

--- carts/views.py
+++ carts/views.py
@@ -1,11 +1,15 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 
 from goods.models import Products
 from .models import Cart
+from .utils import get_user_carts
 
 
-def cart_add(request, product_slug):
-    product = Products.objects.get(slug=product_slug)
+def cart_add(request):
+    product_id = request.POST.get('product_id')
+    product = Products.objects.get(id=product_id)
 
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, products=product)
@@ -16,7 +20,14 @@ def cart_add(request, product_slug):
                 cart.save()
         else:
             Cart.objects.create(user=request.user, products=product, quantity=1)
-    return redirect(request.META['HTTP_REFERER'])
+
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string('carts/includes/included_cart.html', {'carts': user_cart}, request=request)
+    response_data = {
+        'message': 'Товар добавлен',
+        'cart_items_html': cart_items_html
+    }
+    return JsonResponse(response_data)
 
 
 def cart_change(request, product_slug):
